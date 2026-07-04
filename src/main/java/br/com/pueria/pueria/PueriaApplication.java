9704b0e0166dffb44d5db69339fa5e468b3edc14
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-07-04 10:57:19 -0300

    first commit

---
+++ src/main/java/br/com/pueria/pueria/PueriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.pueria.pueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pueria/pueria/PueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pueria.pueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
