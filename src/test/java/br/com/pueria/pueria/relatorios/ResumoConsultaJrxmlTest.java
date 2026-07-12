5459a5602d468a6075712065f2aa736d5ee66154
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-07-12 15:58:15 -0300

    feat: finaliza dados clinicos do resumo pdf

--- src/test/java/br/com/pueria/pueria/relatorios/ResumoConsultaJrxmlTest.java
+++ src/test/java/br/com/pueria/pueria/relatorios/ResumoConsultaJrxmlTest.java
@@ -1,6 +1,7 @@
 package br.com.pueria.pueria.relatorios;
-import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-class ResumoConsultaJrxmlTest { @Test void compilaTemplateDoResumo(){ try(var in=new ClassPathResource("relatorios/resumo-consulta.jrxml").getInputStream()){ assertNotNull(JasperCompileManager.compileReport(in)); } catch(Exception e){ throw new AssertionError(e); } } }
+import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+class ResumoConsultaJrxmlTest { @Test void compilaEGeraPdf(){ try(var in=new ClassPathResource("relatorios/resumo-consulta.jrxml").getInputStream()){ var report=JasperCompileManager.compileReport(in); var parametros=new HashMap<String,Object>(); parametros.put("CONTEUDO","Resumo para consulta"); var print=JasperFillManager.fillReport(report,parametros,new JREmptyDataSource(1)); assertTrue(JasperExportManager.exportReportToPdf(print).length>100); } catch(Exception e){ throw new AssertionError(e); } } }
