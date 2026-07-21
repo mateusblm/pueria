846a0b77e82ebe19fe9a08f386d92d47c6034201
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-07-21 19:14:56 -0300

    Reativa teste explicito do Sentry

--- src/main/java/br/com/pueria/pueria/status/StatusController.java
+++ src/main/java/br/com/pueria/pueria/status/StatusController.java
@@ -1,13 +1,23 @@
 package br.com.pueria.pueria.status;
 
+import io.sentry.Sentry;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
 
     @GetMapping("/api/status")
-    public StatusResponse status() {
+    public StatusResponse status(@RequestParam(defaultValue = "false") boolean sentryTest) {
+        if (sentryTest) {
+            try {
+                throw new Exception("Sentry smoke test: exceção intencional");
+            } catch (Exception exception) {
+                Sentry.captureException(exception);
+                throw new IllegalStateException("Sentry smoke test: exceção intencional", exception);
+            }
+        }
         return new StatusResponse("Pueria API em execução");
     }
 
