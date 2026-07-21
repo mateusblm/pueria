7f208950f44423906dade5771134b1a04638a6c1
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-07-21 19:13:05 -0300

    Remove parametro temporario do Sentry

--- src/main/java/br/com/pueria/pueria/status/StatusController.java
+++ src/main/java/br/com/pueria/pueria/status/StatusController.java
@@ -1,23 +1,13 @@
 package br.com.pueria.pueria.status;
 
-import io.sentry.Sentry;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
 
     @GetMapping("/api/status")
-    public StatusResponse status(@RequestParam(defaultValue = "false") boolean sentryTest) {
-        if (sentryTest) {
-            try {
-                throw new Exception("Sentry smoke test: exceção intencional");
-            } catch (Exception exception) {
-                Sentry.captureException(exception);
-                throw new IllegalStateException("Sentry smoke test: exceção intencional", exception);
-            }
-        }
+    public StatusResponse status() {
         return new StatusResponse("Pueria API em execução");
     }
 
