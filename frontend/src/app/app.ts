eb11daa30d3b56eb8c39536f0a0bf201bea4e4ee
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-07-21 20:27:48 -0300

    Alinha teste do Sentry ao ErrorHandler Angular

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import * as Sentry from '@sentry/angular';
 import { ToastContainerComponent } from './core/toast/toast-container.component';
 
 @Component({
@@ -11,10 +10,7 @@ import { ToastContainerComponent } from './core/toast/toast-container.component'
 })
 export class App {
   // Temporary Sentry verification control; remove after the first frontend event is confirmed.
-  async throwTestError(): Promise<void> {
-    const error = new Error('Sentry Test Error');
-    Sentry.captureException(error);
-    await Sentry.flush(5000);
-    throw error;
+  throwTestError(): void {
+    throw new Error('Sentry Test Error');
   }
 }
